8afe0bcd64d69ceedc2c9217c22ae0262fb1125c
ZilvinasBaltrunas <zilvino@hotmail.com>
2018-08-29 18:04:28 +0300

    added main scss file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import '@/assets/scss/style.scss'
 // Font-awesome //
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee, faEnvelope } from '@fortawesome/free-solid-svg-icons'
